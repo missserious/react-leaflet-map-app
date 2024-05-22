c9bca0acc87a33494ed6946e01b756479a9ad78c
Julia Ernst <Julia_Ernst@gmx.de>
2024-05-22 13:37:27 +0200

    FORM COMPONENT editing
    Improve FORM with lat/long input fields
    Add styles

--- src/components/sidebar/Form.js
+++ src/components/sidebar/Form.js
@@ -1,13 +1,28 @@
 // REACT HOOKS
 import { useState } from "react";
 
+import "./form.css";
+
 export default function Form({ onAddDestinations }) {
+  /*
+   * lat & long
+   * title
+   * duration
+   * date
+   * time
+   * duration
+   * description
+   * rating (stars)
+   *
+   */
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
   const [title, setTitle] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    if (!title) return;
+    if (!title || !longitude || !latitude) return;
 
     const newDestination = {
       uuid: Date.now(),
@@ -15,28 +30,56 @@ export default function Form({ onAddDestinations }) {
       duration: "3",
       location: {
         position: {
-          latitude: 50.516,
-          longitude: 13.636,
+          latitude: latitude,
+          longitude: longitude,
         },
       },
     };
     // console.log(newDestination);
     onAddDestinations(newDestination);
 
+    setLatitude("");
+    setLongitude("");
     setTitle("");
   }
 
   return (
     <>
       <form className="form" onSubmit={handleSubmit}>
-        <h1>Form</h1>
-        <input
-          type="text"
-          placeholder="title..."
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button>Add</button>
+        <div className="form__row">
+          <label className="form__label">Latitude</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="latitude"
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
+          />
+        </div>
+        <div className="form__row">
+          <label className="form__label">Longitude</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="longitude"
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+        </div>
+        <div className="form__row">
+          <label className="form__label">Title</label>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="placeholder"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form__row">
+          <button className="form__button">Add</button>
+        </div>
       </form>
     </>
   );
